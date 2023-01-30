b71a8713e94008268b37eef07489a46bb02a36a7
Anant Hinge <ananthinge135@gmail.com>
2023-01-30 14:11:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PowerOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(a + " raised to the power of " + b + "is:" + Power(a, b));
+            Console.ReadLine();
+        }
+
+        static int Power(int a, int b)
+        {
+            int result = 1;
+
+            for (int i= 0; i <b; i++)
+            {
+                result *= a;
+            }
+            return result;
+        }
+    }
+}
